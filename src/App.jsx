be8eab22af45fcbc34905708f5c554edb097a7ae
JuliaMichaela <juliaperez@yandex.ru>
2025-02-16 19:17:45 +0300

    Created PieChart, GeoChart

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,44 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Chart } from "react-google-charts";
 
 function App() {
+  const data = [
+    ["Task", "Hours per Day"],
+    ["Work", 9],
+    ["Eat", 1],
+    ["Training", 1],
+    ["Stady", 2],
+    ["Housework", 4],
+    ["Sleep", 7],
+  ];
 
+  const options = {
+    title: "My Daily Activities",
+    pieHole: 0.4, // Creates a Donut Chart. Does not do anything when is3D is enabled
+    is3D: true, // Enables 3D view
+    slices: {
+      4: { offset: 0.2 }, // Explodes the second slice
+    },
+    pieStartAngle: 100, // Rotates the chart
+    sliceVisibilityThreshold: 0.02, // Hides slices smaller than 2%
+    legend: {
+      position: "bottom",
+      alignment: "center",
+      textStyle: {
+        color: "#233238",
+        fontSize: 14,
+      },
+    },
+  };
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Chart
+      chartType="PieChart"
+      data={data}
+      options={options}
+      width={"100%"}
+      height={"400px"}
+    />
+  );
 }
 
 export default App
